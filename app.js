4c09ff009b82aee85222358044bc9b3047898c8f
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-21 22:52:38 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // write code here
+        let firstName = 'Chairat';
+        let lastName = 'Sapsanong';
+        let age = 32;
+        let address = '73/1 Samutsakhon';
+        console.log('First Name:', firstName);
+        console.log('Last Name:', lastName);
+        console.log('Age:', age);
+        console.log('Address:', address);
